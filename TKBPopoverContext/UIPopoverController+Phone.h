c0b511afa225878d52dba827e782ad79a6857d46
Yusuke Iwama <yusuke.iwama@gmail.com>
2014-03-08 07:49:01 +0900

    enable popover in iPhone

---
+++ TKBPopoverContext/UIPopoverController+Phone.h
@@ -0,0 +1,15 @@
+//
+//  UIPopoverController+Phone.h
+//  TKBPopoverContext
+//
+//  Created by Yusuke Iwama on 3/8/14.
+//  Copyright (c) 2014 University of Tsukuba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIPopoverController (Phone)
+
++ (BOOL)_popoversDisabled;
+
+@end
---
+++ TKBPopoverContext/UIPopoverController+Phone.m
@@ -0,0 +1,18 @@
+//
+//  UIPopoverController+Phone.m
+//  TKBPopoverContext
+//
+//  Created by Yusuke Iwama on 3/8/14.
+//  Copyright (c) 2014 University of Tsukuba. All rights reserved.
+//
+
+#import "UIPopoverController+Phone.h"
+
+@implementation UIPopoverController (Phone)
+
++ (BOOL)_popoversDisabled
+{
+    return NO;
+}
+
+@end
